f4165d4052dd99b9108df564fe5b318f2a1d7d9d
nilarte <nilart@gmail.com>
2017-03-06 22:09:52 +0530

    more changes

--- Core-Java/src/Jumble.java
+++ Core-Java/src/Jumble.java
@@ -3,38 +3,43 @@ import java.util.*;
 public class Jumble {
 
 	public static void main(String[] args) {
-		char[] s = {'1','2','3','4','5','6'} ;
-		//char[] s = new char[6] ;
-		System.out.println(s);
-		TreeMap<Integer, String> tm= new TreeMap<Integer, String>();
-		tm.put(1, "Chaitanya");
-	    tm.put(1, "Ajeet");
-	    tm.put(11, "Test");
-	    
-	    Set set = tm.entrySet();
-	      // Get an iterator
-	      Iterator i = set.iterator();
-	      // Display elements
-	      while(i.hasNext()) {
-	        Map.Entry me = (Map.Entry)i.next();
-	        System.out.print(me.getKey() + ": ");
-	        System.out.println(me.getValue());
-	      }
-		
+		int[] pos1 = new int[2];
+		ArrayList<String> perm1=permutation("12");
+		SubWord subword1 = new SubWord(pos1, perm1); 
 
+		int[] pos2 = new int[3];
+		String[] perm2 = new String[6];
+		String subword2 = "ARD";
+		permutation("123");
 	}
-	
-	public static void makeWord(HashMap permutation ){
-		TreeMap<Integer, String> tm= new TreeMap<Integer, String>();
-		Set set = permutation.entrySet();
-	      // Get an iterator
-	      Iterator i = set.iterator();
-	      // Display elements
-	      while(i.hasNext()) {
-	        Map.Entry me = (Map.Entry)i.next();
-	        tm.put(Integer.parseInt(me.getKey().toString()),(me.getValue()).toString());
-	      }
+
+	//Taken from http://stackoverflow.com/questions/4240080/generating-all-permutations-of-a-given-string
+	public static ArrayList<String> permutation(String str) { 
+		ArrayList<String> perms = new ArrayList<String>();
+		ArrayList<String> result = permutation("", str, perms); 
+		for (int i=0; i<result.size();i++)
+		{
+			System.out.println(result.get(i));
+		}
+		return result;
 	}
-	
+
+	private static ArrayList<String> permutation(String prefix, String str, ArrayList<String> perms) {
+		int n = str.length();
+		//if (n == 0) System.out.println(prefix);
+		if (n == 0) {
+			//System.out.println(prefix);
+			perms.add(prefix);
+			//return perms;
+		}
+
+		else {
+			for (int i = 0; i < n; i++)
+				permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n),perms);
+		}
+		return perms;
+		
+	}
+
 
 }
