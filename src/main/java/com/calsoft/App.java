f30ddb33f183efa4fd0a44ad8823a1694097a208
shubh9975 <68888680+shubh9975@users.noreply.github.com>
2022-09-05 20:21:24 +0530

    update a source code loop

--- src/main/java/com/calsoft/App.java
+++ src/main/java/com/calsoft/App.java
@@ -4,12 +4,14 @@ package com.calsoft;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+      while (true) {
         System.out.println( "Hello World!" );
         //added changes to test scm build fire 2
         //testing webhooks3
+      }
     }
 }
