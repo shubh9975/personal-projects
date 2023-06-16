15ab8a66954d3b22779fb6eb00b2f7869efb04cd
nilarte <nilart@gmail.com>
2023-06-16 16:15:24 +0530

    Update AppTest.java

--- src/test/java/com/calsoft/AppTest.java
+++ src/test/java/com/calsoft/AppTest.java
@@ -1,39 +1,13 @@
 package com.calsoft;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest {
+    @Test
+    public void getMessage() {
+        App app = new App();
+        assertEquals("Default message is valid", app.getMessage(), "Hello World!");
     }
 }
-//Just to show git demo
